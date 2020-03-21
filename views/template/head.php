1a43f92ceed0489245270c39458f2efeda9ca235
costi <cnecula20@yahoo.ro>
2020-03-21 23:56:15 +0200

    start project

---
+++ views/template/head.php
@@ -0,0 +1,10 @@
+<head>
+    <title>NecuFrame</title>
+
+    <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap-grid.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap-reboot.css">
+
+    <script src="/assets/js/jquery.min.js/"></script>
+    <script src="/assets/js/bootstrap.js/"></script>
+</head>
