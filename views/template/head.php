a6bd19c71ed9a816137e87126b869b82a7f90dec
costi <cnecula20@yahoo.ro>
2020-03-24 23:44:17 +0200

    aaa

--- views/template/head.php
+++ views/template/head.php
@@ -4,14 +4,14 @@
     <script src="/assets/js/jquery.min.js/"></script>
 
 
-    <link rel="stylesheet" type="text/css" href="<?= __DIR__ ?>/assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="<?= __DIR__ ?>/assets/css/fontawesome.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= __DIR__ ?>/assets/css/bootstrap-grid.css">
-    <link rel="stylesheet" type="text/css" href="<?= __DIR__ ?>/assets/css/bootstrap-reboot.css">
-    <link rel="stylesheet" type="text/css" href="<?= __DIR__ ?>/assets/css/style.css"
+    <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/fontawesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap-grid.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css"
     <link href="https://fonts.googleapis.com/css?family=Raleway:500i&display=swap" rel="stylesheet">
 
-    <script src="<?= __DIR__ ?>/assets/js/bootstrap.js/"></script>
-    <script src="<?= __DIR__ ?>/assets/js/script.js"></script>
+    <script src="/assets/js/bootstrap.js/"></script>
+    <script src="/assets/js/script.js"></script>
 
 </head>
