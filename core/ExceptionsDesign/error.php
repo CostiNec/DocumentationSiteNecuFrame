ee502e2a4784e31837690322546d3da66d45ea6e
costi <cnecula20@yahoo.ro>
2020-03-25 05:27:16 +0200

    update to new version of frame

---
+++ core/ExceptionsDesign/error.php
@@ -0,0 +1,14 @@
+<head>
+    <title>Error</title>
+
+</head>
+<style>
+    <?= include __DIR__. '/css/error.css'?>
+</style>
+<div class="row">
+        <div class="error-position">
+            <p><?= $errorCode ?></p>
+            <p><?= $message ?></p>
+        </div>
+        <img src="assets/img/errorImage.png">
+</div>
